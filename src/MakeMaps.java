fe0690a46a77b822350718bdc5d992de853e628d
Christopher Wells <cwells21@masonlve.gmu.edu>
2018-11-15 12:27:58 -0500

    Map Generator Final Version

---
+++ src/MakeMaps.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+
+public class MakeMaps {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		
+		addRoom test = new addRoom();
+	}
+
+}
